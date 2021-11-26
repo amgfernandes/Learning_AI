b43d5b707771f338ca6d43feff6f2978be1e9896
amgfernandes <miguel.fernandes@dzne.de>
2021-11-26 13:16:03 +0100

    denoising example

--- Intro_Autoencoders.py
+++ Intro_Autoencoders.py
@@ -4,7 +4,6 @@
 
 '''Libraries'''
 
-
 #env:tensorflow
 
 import matplotlib.pyplot as plt
@@ -46,7 +45,10 @@ for i in range(num_row*num_col):
 plt.tight_layout()
 plt.show()
 # %%
-### First example: Basic autoencoder
+##################################
+# First example: Basic autoencoder
+##################################
+
 '''Define an autoencoder with two Dense layers: an encoder, which compresses 
 the images into a 64 dimensional latent vector, and a decoder, that reconstructs
 the original image from the latent space.'''
@@ -109,5 +111,123 @@ for i in range(n):
   plt.gray()
   ax.get_xaxis().set_visible(False)
   ax.get_yaxis().set_visible(False)
+plt.show()
+# %%
+##################################
+# Second example: Image denoising
+##################################
+(x_train, _), (x_test, _) = fashion_mnist.load_data()
+
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
+
+x_train = x_train[..., tf.newaxis]
+x_test = x_test[..., tf.newaxis]
+
+print(x_train.shape)
+
+#%%
+'''Adding random noise to the images'''
+noise_factor = 0.2
+x_train_noisy = x_train + noise_factor * tf.random.normal(shape=x_train.shape) 
+x_test_noisy = x_test + noise_factor * tf.random.normal(shape=x_test.shape) 
+
+x_train_noisy = tf.clip_by_value(x_train_noisy, clip_value_min=0., clip_value_max=1.)
+x_test_noisy = tf.clip_by_value(x_test_noisy, clip_value_min=0., clip_value_max=1.)
+
+
+
+# %%
+n = 10
+plt.figure(figsize=(20, 2))
+for i in range(n):
+    ax = plt.subplot(1, n, i + 1)
+    plt.title("original + noise")
+    plt.imshow(tf.squeeze(x_test_noisy[i]))
+    plt.gray()
+plt.show()
+
+#%%
+
+class Denoise(Model):
+ def __init__(self):
+    super(Denoise, self).__init__()
+    self.encoder = tf.keras.Sequential([
+      layers.Input(shape=(28, 28, 1)),
+      layers.Conv2D(16, (3, 3), activation='relu', padding='same', strides=2),
+      layers.Conv2D(8, (3, 3), activation='relu', padding='same', strides=2)])
+
+    self.decoder = tf.keras.Sequential([
+      layers.Conv2DTranspose(8, kernel_size=3, strides=2, activation='relu', padding='same'),
+      layers.Conv2DTranspose(16, kernel_size=3, strides=2, activation='relu', padding='same'),
+      layers.Conv2D(1, kernel_size=(3, 3), activation='sigmoid', padding='same')])
+
+ def call(self, x):
+    encoded = self.encoder(x)
+    decoded = self.decoder(encoded)
+    return decoded
+
+autoencoder = Denoise()
+
+print(autoencoder)
+# %%
+autoencoder.compile(optimizer='adam', loss=losses.MeanSquaredError())
+
+#%%
+
+autoencoder.fit(x_train_noisy, x_train,
+                epochs=10,
+                shuffle=True,
+                validation_data=(x_test_noisy, x_test))
+# %%
+autoencoder.encoder.summary()
+# %%
+autoencoder.decoder.summary()
+# %%
+encoded_imgs = autoencoder.encoder(x_test).numpy()
+decoded_imgs = autoencoder.decoder(encoded_imgs).numpy()
+# %%
+'''Noisy vs decoded'''
+n = 10
+plt.figure(figsize=(20, 4))
+for i in range(n):
+
+    # display original + noise
+    ax = plt.subplot(2, n, i + 1)
+    plt.title("original + noise")
+    plt.imshow(tf.squeeze(x_test_noisy[i]))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    # display reconstruction
+    bx = plt.subplot(2, n, i + n + 1)
+    plt.title("reconstructed")
+    plt.imshow(tf.squeeze(decoded_imgs[i]))
+    plt.gray()
+    bx.get_xaxis().set_visible(False)
+    bx.get_yaxis().set_visible(False)
+plt.show()
+# %%
+
+'''Original vs decoded'''
+n = 10
+plt.figure(figsize=(20, 4))
+for i in range(n):
+    # display original
+    ax = plt.subplot(2, n, i + 1)
+    plt.imshow(x_test[i])
+    plt.title("original")
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    # display reconstruction
+    bx = plt.subplot(2, n, i + n + 1)
+    plt.title("reconstructed")
+    plt.imshow(tf.squeeze(decoded_imgs[i]))
+    plt.gray()
+    bx.get_xaxis().set_visible(False)
+    bx.get_yaxis().set_visible(False)
 plt.show()
 # %%
